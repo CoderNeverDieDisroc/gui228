b6e5c21fc1e5779d38181f6eab6af854f62090f0
CoderNeverDieDisroc <fuckingmotherbeathc@mail.ru>
2024-11-04 15:08:32 +0300

    Update guishka228.lua

--- guishka228.lua
+++ guishka228.lua
@@ -2790,7 +2790,7 @@ end
 function library:Load(options)
 	options = options or {}
 	utility.table(options)
-	local name = "Blume.cc"
+	local name = "Blume.cc [alkad beta]"
 	local sizeX = options.sizex or 450
 	local sizeY = options.sizey or 500
 	local theme = options.theme and options.theme or "AirHub"
